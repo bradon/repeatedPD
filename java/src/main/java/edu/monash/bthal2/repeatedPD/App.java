50254cda8205e4060a9078e849209dc1acd85d50
Bradon <bradon.hall@iinet.net.au>
2014-08-08 14:36:50 +1000

    Copied existing App class

--- java/src/main/java/edu/monash/bthal2/repeatedPD/App.java
+++ java/src/main/java/edu/monash/bthal2/repeatedPD/App.java
@@ -1,13 +1,51 @@
 package edu.monash.bthal2.repeatedPD;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.io.IOException;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+
+import edu.monash.bthal2.repeatedPD.simulation.LookupPlayerLengthOnePayoffSimulation;
+import edu.monash.bthal2.repeatedPD.simulation.PDAPayoffSimulation;
+
+public class App {
+	@Parameter(names = { "-file", "-f" }, description = "Name of the json file")
+	private String file;
+
+	enum SimulationType {
+		PAYOFF, PDAPAYOFF, FSAPAYOFF
+	}
+
+	@Parameter(names = { "-type", "-t" }, description = "Type of simulation PAYOFF", required = true)
+	private SimulationType type;
+
+	public static void main(String[] args) throws IOException {
+
+		App app = new App();
+		// Parsing
+		JCommander commander = new JCommander(app);
+		try {
+			commander.parse(args);
+		} catch (ParameterException e) {
+			System.out.println(e.getMessage());
+			commander.usage();
+			return;
+		}
+
+		switch (app.type) {
+		case PAYOFF:
+			LookupPlayerLengthOnePayoffSimulation.runOnce(app.file);
+			break;
+		case PDAPAYOFF:
+			PDAPayoffSimulation.runOnce(app.file);
+			break;
+		case FSAPAYOFF:
+			break;
+		default:
+			System.out.println("Simulation not implemented");
+			break;
+
+		}
+	}
 }
