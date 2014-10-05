2be2e03f110f58161e20514bfb290156ea222f43
Bradon <bradon.hall@iinet.net.au>
2014-10-05 16:14:46 +1100

    Basic testing of flipresult

--- java/src/test/java/edu/monash/bthal2/repeatedPD/DPDA/DPDATest.java
+++ java/src/test/java/edu/monash/bthal2/repeatedPD/DPDA/DPDATest.java
@@ -6,12 +6,29 @@ import org.junit.Test;
 
 import com.evolutionandgames.repeatedgames.evolution.Action;
 
+import edu.monash.bthal2.repeatedPD.DPDA.Exception.MultipleTransitionException;
+
 public class DPDATest {
 
 	@Test
-	public void testDefaultConstructor() {
+	public void testDefaultConstructor() throws MultipleTransitionException {
 		DPDA test = new DPDA();
 		assertTrue(test.currentAction() == Action.DEFECT);
-
+		test.readInput(Action.DEFECT);
+		assertTrue(test.currentAction() == Action.DEFECT);
+		test.readInput(Action.COOPERATE);
+		assertTrue(test.currentAction() == Action.DEFECT);
 	}
+	
+	@Test
+	public void testFlipResultBasic() throws MultipleTransitionException {
+		DPDA test = new DPDA();
+		test.flipResult=true;
+		assertTrue(test.currentAction() == Action.COOPERATE);
+		test.readInput(Action.DEFECT);
+		assertTrue(test.currentAction() == Action.COOPERATE);
+		test.readInput(Action.COOPERATE);
+		assertTrue(test.currentAction() == Action.COOPERATE);
+	}
+	
 }
